878b64c1ea15d5164f9c1fc8d6602a8078d9b50b
wodl0504 <wodl0504@naver.com>
2020-02-29 18:03:44 +0900

    중간 저장

--- src/App.js
+++ src/App.js
@@ -3,43 +3,22 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
+  // Render : componentWillMount() -> render() -> componentDidMount()
+  // Updaste componentWillReceiveProps() -> shouldComponentUpdate() -> componentWiilUpldate() -> render() -> componentDidUpdate()
 
-  /**
-   * state가 바뀔때마다 render 발생
-   * 
-   */
-  state = {
-  }
-
-  componentDidMount(){
-   setTimeout(() => { 
-      this.setState({
-        movies : [
-          {
-            title: "기생충",
-            poster: "/src/images/gisaengchong.jpg"
-          },
-          {
-            title: "시동",
-            poster: "/src/images/sidong.jpg"
-          },
-          {
-            title: "Kingdom",
-            poster: "/src/images/kingdom.jpg"
-          },
-          {
-            title: "6 Underground",
-            poster: "/src/images/6underground.jpg"
-          },
-          {
-            title: "Harry Potter",
-            poster: "/src/images/harrypotter.jpg"
-          }
-        ]
-      })
-   },2500)
-  }
-
+   state ={}
+   componentDidMount(){
+    const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating';
+    fetch(url)
+    .then(res => res.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err));
+   }
+/**
+ * Promise
+ * javascript concept
+ * 비동기 프로그래밍이 가능. 
+ */
   _renderMovies = () => {
    const movies = this.state.movies.map((movie,index) => {
       return <Movie title={movie.title} poster={movie.poster} key={index}/>

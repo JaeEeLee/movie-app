113e7b6e806abb63ec9b8d56726a4668371cde5c
wodl0504 <wodl0504@naver.com>
2020-03-23 08:03:28 +0900

    20200322 ajax 대신 json dummy 활용 (cors 문제)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie'
+import Axios from 'axios';
 
 class App extends Component {
   // Render : componentWillMount() -> render() -> componentDidMount()
   // Updaste componentWillReceiveProps() -> shouldComponentUpdate() -> componentWiilUpldate() -> render() -> componentDidUpdate()
 
-   state ={}
-   componentDidMount(){
-    const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating';
-    fetch(url)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err));
+   state ={
+     movies : []
    }
+   componentDidMount(){
+     this._getMovies();
+    }
 /**
  * Promise
  * javascript concept
  * 비동기 프로그래밍이 가능. 
  */
+
   _renderMovies = () => {
-   const movies = this.state.movies.map((movie,index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index}/>
-    })
+    const movies = this.state.movies.map(movie => {
+      //component의 key를 인덱스로 사용하는 것은 느리기때문에 지양하는 방법
+        return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id }/>
+      })
     return movies;
   }
+
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi() {
+    const apiUrl = 'dummy/list_movies.json';
+  /* //  CORS 공부할것..
+  const url = 'https://yts.ag/api/v2/list_movies.json?sort_by=rating';
+   fetch(url)
+     .then(res => res.json())
+     .then(json => console.log(json))
+     .catch(err => console.log(err));
+     */
+    return Axios.get(apiUrl)
+      .then(json => json.data.data.movies)
+      .catch(error => {
+        console.log(error);
+      })
+  }
+ 
 
   render() {
     return (
